653ebd0a386e34785fd8c9b52458a185446fd5f6
topisteronyango <t.onyango@alustudent.com>
2022-11-09 12:59:51 +0200

    elevator

--- elevator.c
+++ elevator.c
@@ -6,6 +6,7 @@
 
 int request_elevator() {
   int start_floor, end_floor;
+  //seed time
   srand(time(NULL));
   // pthread_t floors[end_floor];
   while(start_floor == end_floor){
@@ -14,6 +15,19 @@ int request_elevator() {
   }
     printf("The start floor: %d\n", start_floor);
   return end_floor;
+}
+
+//Function to create threads
+int create_thread(){
+  int threads, num_threads = 5;
+  //initializing mutex
+  pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_t thread[num_threads];
+  sleep(2);
+  for(int i=0; i < num_threads; i++){
+    threads = pthread_create(&thread[num_threads], NULL, NULL, NULL);
+  }
+
 }
 
 int main(void) {
