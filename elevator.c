d9e8bc1f682ed549297c65643261bec42f861901
MillicentMal <millicentmalinga19@gmail.com>
2022-11-09 12:49:22 +0200

    stash

--- elevator.c
+++ elevator.c
@@ -4,17 +4,41 @@
 #include <time.h>
 #include <unistd.h>
 
-int request_elevator() {
-  int start_floor, end_floor;
+int generate_floors() {
+  int start_floor = 0, end_floor= 0;
   srand(time(NULL));
   // pthread_t floors[end_floor];
-  start_floor = rand() % 9;
-  end_floor = rand() % 9;
+  while(start_floor == end_floor){
+  start_floor = rand() % 8;
+  end_floor = rand() % 8;
+  }
     printf("The start floor: %d\n", start_floor);
   return end_floor;
 }
 
+int abd(int a, int b) {
+    /*
+    * Helper function to find the absolute difference between two numbers
+    *
+    */
+
+    if (a > b) {
+    return a - b;
+    }
+    else {
+        return b -a;
+    }
+}
+
+int travel_time(int start_floor, int end_floor) {
+    int time = (abd(end_floor, start_floor)) * 2;
+    return time;
+}
+
+
 int main(void) {
-  printf("this is the end floor %d", request_elevator());
+  printf("this is the end floor %d\n", generate_floors());
+  printf("Time between floors is: %d", travel_time(4, 2));
   return 0;
 }
+
