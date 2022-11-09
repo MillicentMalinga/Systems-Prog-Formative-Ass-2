0b87bc4a9364ab51fda2f0e4d1d5fc947397ac84
topisteronyango <t.onyango@alustudent.com>
2022-11-09 12:40:09 +0200

    add thread function

--- elevator.c
+++ elevator.c
@@ -1,8 +1,10 @@
-// #include <pthread.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+
+pthread_mutex_t mymutex;
 
 int request_elevator() {
   int start_floor, end_floor;
@@ -14,7 +16,21 @@ int request_elevator() {
   return end_floor;
 }
 
+//Function to create threads
+int create_thread(){
+  int threads, num_threads = 5;
+  //initializing mutex
+  pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+  pthread_t thread[num_threads];
+  sleep(2);
+  for(int i=0; i < num_threads; i++){
+    threads = pthread_create(&thread[num_threads], NULL, NULL, NULL);
+  }
+
+}
+
 int main(void) {
-  printf("this is the end floor %d", request_elevator());
+  printf("this is the end floor %d\n", request_elevator());
+  create_thread();
   return 0;
 }
