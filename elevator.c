d0095f4d6c5cbaa869204bc7c49053887fa254be
MillicentMalinga <90625689+MillicentMalinga@users.noreply.github.com>
2022-11-14 21:52:18 +0200

    Update elevator.c

--- elevator.c
+++ elevator.c
@@ -1,134 +1,126 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
-#include <ctype.h>
+#include <errno.h>
+#include <time.h>
 
-struct Floors {
-    int start_floor, end_floor;
-};
-struct Floors generate_floors() {
-  struct Floors floors = {0, 0};
-  //seed time
-  srand(time(NULL));
-  // pthread_t floors[end_floor];
-  while(floors.start_floor == floors.end_floor){
-  floors.start_floor = rand() % 8;
-  floors.end_floor = rand() % 8;
-  // }
-  //   printf("The start floor: %d\n", start_floor);
-  return floors;
-}
-}
-void *passenger_status(){
-    printf("Passenger called elevator.\n");
+typedef struct Rider{
+   int start, end, id;
+}Rider;
+#define THREAD_NUM 10
+// static  struct Elevator{
+//    int capacity;
+//    int id;
+//    int current_floor;  
+//    Rider *rider;
+// }Elevator;
 
-  }
-
-// helper function for absolute difference
-int abd(int a, int b) {
-    /*
-    * Helper function to find the absolute difference between two numbers
-    *
-    */
-
-    if (a > b) {
-    return a - b;
-    }
-    else {
-        return b -a;
+  int waiters = 0;
+    int waiting[20];
+pthread_mutex_t elevatorMutex[2];
+pthread_barrier_t collectBarrier;
+int currentFloor[2] = { 1, 1 };
+int picked_up[10];
+int capacity;
+void collectUsers(){
+    printf("How many people are waiting for the elevators?\n");
+    scanf("%d", &waiters);
+    printf("Which floors are they waiting on?\n");
+    for (int i = 0; i < waiters; i++){
+        scanf("%d", &waiting[i]);
     }
 }
+// void *offload(Elevator *elevator){
+//    int i;
+//    for(i = 0; i < elevator->capacity; i++){
+//       if(elevator->rider[i].end == elevator->current_floor){
+//          printf("Rider %d offloaded at floor %d", elevator->rider[i].id, elevator->current_floor);}
+//         elevator.capacity--;
+// }
+// }
+void* travel(void* args) {
+    int direction = 1;
 
+    for (int i = 0; i < 2; i++) {
+        if (pthread_mutex_trylock(&elevatorMutex[i]) == 0) {
+            while(1){
+                
+                printf("\nElevator %d current floor: %d\n\n", i+1, currentFloor[i]);
+                for (int i = 0; i < waiters; i++){
+                    if (waiting[i] == currentFloor[i] && capacity < 10){
 
-// calculate wait time for each thread
-
-int travel_time(int start_floor, int end_floor){
- int travel_time = abd(end_floor, start_floor) * 2;
- return travel_time;
-
-}
-
-
-
-void elevator_process(){
-  srand(time(NULL));
-  pthread_t passengers[5];
-int capacity = 5;
-int state = 0; // state of the elevator. 0 for idle and 1  for running
-int direction = 1; // direction the elevator is going in. 1 for up and -1 for down.
-int current_floor = 0; // the floors the elevator is in
-int final_destination = 0; // the final floor tht the elevator is going to. 
-int current_number = 0;  // number of people in the elevator.
-int passengers_in_waiting = rand() % 6;
-int available_space = capacity - current_number;
-int destination_floors[5];
-int times[5], threads;
-printf("You have %d passengers waiting\n", passengers_in_waiting);
-printf("The elevator has %d passengers\n", current_number);
-printf("The available space %d\n", available_space);
-
-if (passengers_in_waiting > available_space) {
-  printf("Only space for %d people\n", available_space);
-for (int i = 0; i < available_space; i++){
-   srand(time(NULL));
- struct Floors floors = generate_floors();
-  int wait_time = travel_time(floors.start_floor, floors.end_floor);
-    threads = pthread_create(&passengers[i], NULL,&passenger_status, NULL);
-    if (threads != 0) {
-      printf("Something went wrong!");
+                        printf("Rider %d is waiting on floor %d\n", i+1, currentFloor[i]);
+                        printf("Picking up rider %d\n", i+1);
+                        printf("Please enter your destination floor\n");
+                        sleep(1);
+                        scanf("%d", &picked_up[capacity]);
+                        capacity++;
+                        waiting[-1];
+                        
+                    }
+                for (int i = 0; i < capacity; i++) {
+                    if (picked_up[i] == currentFloor[i]) {
+                        printf("Rider %d offloaded at floor %d\n", i+1, currentFloor[i]);
+                        picked_up[-1];
+                        capacity--;
+                    }
+                }
+                }
+                printf("-------------------------------------------------------\n");
+        if (direction == 0){
+        printf("\n----------------GOING DOWN------------------\n");
+        }
+        else if(direction == 1){
+            printf("\n--------------- GOING UP------------------\n");
+        }
+          sleep(2);
+            if (currentFloor[i] == 1 && currentFloor[i] != 8){
+                direction = 1;
+                currentFloor[i]++;
+            }
+           else if (currentFloor[i] < 8 && direction == 1){
+                currentFloor[i]++;
+            }
+            else if (currentFloor[i] > 1 && currentFloor[i] == 8 || direction == 0){
+                direction = 0;
+            currentFloor[i]--;
+            }
+         
+            }
+            pthread_mutex_unlock(&elevatorMutex[i]);
+        } else {
+            if (i == 2) {
+                i=0;
+            }
+        }
     }
-  }
-  }
-  else if (passengers_in_waiting <= available_space) {
-    printf("Welcome on board\n");
-    for (int i = 0; i < passengers_in_waiting; i++){
-       srand(time(NULL));
- struct Floors floors = generate_floors();
- printf("start: %d, end: %d", floors.start_floor, floors.end_floor);
-  int wait_time = travel_time(floors.start_floor, floors.end_floor);
-    threads = pthread_create(&passengers[i], NULL,&passenger_status, NULL);
-    if (threads != 0) {
-      printf("Something went wrong!");
+
+}
+
+
+int main(int argc, char* argv[]) {
+    collectUsers();
+    srand(time(NULL));
+    pthread_t th[10];
+    for (int i = 0; i < 2; i++) {
+        pthread_mutex_init(&elevatorMutex[i], NULL);
     }
-    printf("Your destination will be reached in %d\n", wait_time);
-  }
-
-  }
-}
-
-int request_elevator(int start_floor, int end_floor, int elevator_direction){
-  int passenger_direction;
-  if (end_floor - start_floor < 0) {
-  passenger_direction = 0;
-  }
-  else if (end_floor - start_floor > 0){
-    passenger_direction =  1;
-  }
-  else {
-    printf("Destination invalid\n")
-  }
-  if 
-  return wait_time;
-}
-
-
-int main(void) {
-      struct Floors floors = generate_floors();
-    
-      elevator_process();
-  return 0;
-
-  }
-
- 
-
-
-// // Function to create threads
-// int create_thread(){
-//   int threads, capacity = 5;
-  //initializing mutex
-  // pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
-  // pthread_t thread[capacity];
+    for (int i = 0; i < 10; i++) {
+        if (pthread_create(&th[i], NULL, &travel, NULL) != 0) {
+            perror("Failed to create thread");
+        }
+    }
   
+    for (int i = 0; i < 10; i++) {
+     
+
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to join thread");
+        }
+    }
+    for (int i = 0; i < 2; i++) {
+        pthread_mutex_destroy(&elevatorMutex[i]);
+    }
+    return 0;
+}
